6146ef916c0a7f09adba1f6c273db916c8301265
Ikey Doherty <ikey@aerynos.com>
2025-03-15 02:43:06 +0000

    backend: Skeletal crate for grpc backend
    May seem overkill but frankly it's the only "good"
    way to handle this. No IPC split no cookies.
    
    Signed-off-by: Ikey Doherty <ikey@aerynos.com>

---
+++ backend/src/main.rs
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: Copyright © 2025 Serpent OS Developers
+// SPDX-FileCopyrightText: Copyright © 2025 AerynOS Developers
+//
+// SPDX-License-Identifier: MPL-2.0
+
+fn main() {
+    println!("Hello, world!");
+}
