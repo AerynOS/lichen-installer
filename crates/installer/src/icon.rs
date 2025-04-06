c3486c6b26078123355f8f3067c48c3a72f33bd4
Ikey Doherty <ikey@aerynos.com>
2025-04-06 22:40:13 +0100

    installer: Add Emoji variant and Display impl
    Signed-off-by: Ikey Doherty <ikey@aerynos.com>

--- crates/installer/src/icon.rs
+++ crates/installer/src/icon.rs
@@ -3,6 +3,8 @@
 //
 // SPDX-License-Identifier: MPL-2.0
 
+use std::fmt::{Display, Formatter};
+
 /// An icon to display for a step
 pub enum Icon {
     /// A standard icon (free desktop icon name)
@@ -16,6 +18,20 @@ pub enum Icon {
         fallback: String,
     },
 
+    /// An emoji icon
+    Emoji(String),
+
     /// A path to a custom icon, with a specified size
     Custom(String, u32, u32),
+}
+
+impl Display for Icon {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Icon::Standard(name) => write!(f, "{}", name),
+            Icon::Symbolic { name, fallback } => write!(f, "{} (fallback: {})", name, fallback),
+            Icon::Custom(path, width, height) => write!(f, "{} ({}x{})", path, width, height),
+            Icon::Emoji(emoji) => write!(f, "{}", emoji),
+        }
+    }
 }
